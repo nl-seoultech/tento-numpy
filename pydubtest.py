8329f9ed2bfa43c417573b1fe1ac4f2706e60c22
Kang HyoJun <admire9@gmail.com>
2014-08-11 17:39:08 +0900

    modify

--- pydubtest.py
+++ pydubtest.py
@@ -8,11 +8,11 @@ import matplotlib.pyplot as plt
 
 sound = AudioSegment.from_mp3(argv[1])
 raw_data = sound._data
-print(type(raw_data))
+raw_data = raw_data[int(argv[2]):int(argv[3])]
 xs = bytearray(raw_data)
 x = linspace(0, 274, len(raw_data))
 freqs = fftfreq(len(raw_data), x[1] - x[0])
 sp = fft(xs)
-plt.plot(freqs, sp.real, freq, sp.imag)
+plt.plot(freqs, sp.real, freqs, sp.imag)
 plt.savefig('./abc.png')
 
